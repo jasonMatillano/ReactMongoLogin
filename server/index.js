7bac08dad95fa168e88303d3b6782014af278a98
Jason <mat.lopez072023@gmail.com>
2024-09-21 02:45:07 +0800

    005AddedEmployeeModel

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const EmployeeModel = require("./models/Employee");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/employee")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+.connect("mongodb://127.0.0.1:27017/employee")
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
 
+app.post("/register", (req, res) => {
+    EmployeeModel.create(req.body)
+    .then((data) => res.send(data))
+    .catch((err) => res.send(err));
+})
 
 app.listen(3001, () => {
-    console.log("Server started on port 5000");
+    console.log("Server started on port 3001");
 })
